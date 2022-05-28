ef5dfa771097fdbe965a00e6fb1e9beb19e7d17d
ChamXE <xuaner11221@outlook.com>
2022-05-28 15:31:29 +0800

    First Q6 attempt

---
+++ Nintex/src/nintex/Q6.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nintex;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author XuanEr
+ */
+public class Q6 {
+	
+	public static void main(String[] args) {
+		
+		HashMap<Character, Integer> map = new HashMap<>();
+		String L1 = "peer";
+		String L2 = "deer";
+		for(int i=0; i<L1.length(); i++){
+			if(map.get(L1.charAt(i)) == null){
+				map.put(L1.charAt(i), 1);
+			}
+			else {
+				int curValue = map.get(L1.charAt(i));
+				curValue++;
+				map.replace(L1.charAt(i), curValue);
+			}
+		}
+		int count = 0;
+		for(int i=0; i<L2.length(); i++){
+			if(map.get(L2.charAt(i)) != null && map.get(L2.charAt(i)) != 0){
+				count++;
+				int curValue = map.get(L2.charAt(i));
+				curValue--;
+				map.replace(L2.charAt(i), curValue);
+			}
+		}
+		System.out.println(count);
+	}
+}
